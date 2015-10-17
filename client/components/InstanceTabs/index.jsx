7ec95e532af9a19a5fa5df139e92547fa7b278b7
luin <i@zihua.li>
2015-10-17 12:08:16 +0800

    Hide the tabar when there's only on instance

--- client/components/InstanceTabs/index.jsx
+++ client/components/InstanceTabs/index.jsx
@@ -7,26 +7,28 @@ import { Tab, Tabs } from './draggable-tab';
 
 class InstanceTabs extends React.Component {
   render() {
-    return <Tabs style={ { display: this.props.instances.count() === 1 ? 'none' : 'block' } }
-      onTabAddButtonClick={() =>
-        store.dispatch(action('addInstance'))
-      }
-      onTabSelect={(key) =>
-        store.dispatch(action('selectInstance', key))
-      }
-      onTabClose={(key) =>
-        store.dispatch(action('delInstance', key))
-      }
-      onTabPositionChange={(from, to) =>
-        store.dispatch(action('moveInstance', { from, to }))
-      }
-      selectedTab={ this.props.activeInstanceKey }
-      tabs={
-        this.props.instances.map(instance => {
-          return (<Tab key={instance.get('key')} title={instance.get('host')} ></Tab>);
-        }).toJS()
-      }
-    />;
+    return <div style={ { display: this.props.instances.count() === 1 ? 'none' : 'block' } }>
+      <Tabs
+        onTabAddButtonClick={() =>
+          store.dispatch(action('addInstance'))
+        }
+        onTabSelect={(key) =>
+          store.dispatch(action('selectInstance', key))
+        }
+        onTabClose={(key) =>
+          store.dispatch(action('delInstance', key))
+        }
+        onTabPositionChange={(from, to) =>
+          store.dispatch(action('moveInstance', { from, to }))
+        }
+        selectedTab={ this.props.activeInstanceKey }
+        tabs={
+          this.props.instances.map(instance => {
+            return (<Tab key={instance.get('key')} title={instance.get('host')} ></Tab>);
+          }).toJS()
+        }
+      />
+    </div>;
   }
 }
 
