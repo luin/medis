36abbd30ff12945bc5c62c52e8e90aaf782b46e5
luin <i@zihua.li>
2015-10-23 00:04:49 +0800

    Add key filters

--- client/components/Main/Database/index.jsx
+++ client/components/Main/Database/index.jsx
@@ -3,22 +3,28 @@
 import React from 'react';
 import store from '../../../store';
 import action from '../../../actions';
-import KeySelector from './KeySelector';
+// import KeySelector from './KeySelector';
+import { Table, Column } from 'fixed-data-table';
 
 class Database extends React.Component {
   constructor() {
     super();
-    this.state = { keys: [] };
+    this.state = { keys: [], sidebarWidth: 400 };
   }
 
   componentDidMount() {
     const redis = this.props.redis;
     console.log(redis);
 
-    redis.scan('0', 'MATCH', 'fh:*', 'COUNT', '50', (err, res) => {
+    redis.scan('0', 'MATCH', 'fh:*', 'COUNT', '200', (err, res) => {
       console.log(err, res);
-      this.setState({
-        keys: res[1]
+      Promise.all(res[1].map(key => {
+        return Promise.all([key, redis.type(key)]);
+      })).then(keys => {
+        console.log(keys);
+        this.setState({
+          keys
+        });
       });
     });
   }
@@ -28,21 +34,40 @@ class Database extends React.Component {
   }
 
   render() {
+    function rowGetter(rowIndex) {
+      return this.state.keys[rowIndex];
+    }
     return <div className="pane-group">
-      <aside className="pane pane-sm sidebar">
-        <KeySelector
-          patterns={this.props.patterns}
-          connectionKey={this.props.connectionKey}
-          onSelect={this.handleSelectPattern.bind(this)}
-        />
-      </aside>
-      <aside className="pane pane-sm sidebar">
-        {
-          this.state.keys.map(key => {
-            return <p>{key}</p>;
-          })
-        }
-      </aside>
+      <div className="pane sidebar" style={ { flex: 'none', width: this.state.sidebarWidth } }>
+        <div className="pattern-input">
+          <span className="icon icon-search"></span>
+          <input type="search" className="form-control" placeholder="Key name or patterns" />
+        </div>
+        <div className="pattern-table">
+          <Table
+            rowHeight={30}
+            rowGetter={rowGetter.bind(this)}
+            rowsCount={this.state.keys.length}
+            width={this.state.sidebarWidth}
+            height={500}
+            headerHeight={30}>
+            <Column
+              label="name"
+              width={300}
+              dataKey={0}
+              isResizable={true}
+            />
+            <Column
+              label="type"
+              width={100}
+              dataKey={1}
+              isResizable={true}
+            />
+          </Table>
+        </div>
+      </div>
+      <div className="pane ">
+      </div>
       <div className="pane">
         <button onClick={() =>
           store.dispatch(action('connect'))
