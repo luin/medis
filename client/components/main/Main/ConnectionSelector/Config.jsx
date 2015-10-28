4aabb7a56f6db99f2dcfdfa3691a095298d9a96e
luin <i@zihua.li>
2015-10-28 10:30:04 +0800

    Add two buttons

--- client/components/main/Main/ConnectionSelector/Config.jsx
+++ client/components/main/Main/ConnectionSelector/Config.jsx
@@ -30,28 +30,28 @@ class Config extends React.Component {
         }
       } className="nt-form" style={ { width: 500, margin: '60px auto 0' } }>
         <div className="nt-form__row" style={ { display: this.props.favorite ? 'block' : 'none' } }>
-          <label htmlFor="name">Name</label>
+          <label htmlFor="name">Name:</label>
           <input type="text" id="name" value={this.getProp('name')} placeholder="Bookmark name" />
         </div>
         <div className="nt-form__row">
-          <label htmlFor="host">Redis Host</label>
+          <label htmlFor="host">Redis Host:</label>
           <input type="text" id="host" value={this.getProp('host')} placeholder="localhost" />
         </div>
         <div className="nt-form__row">
-          <label htmlFor="port">Port</label>
+          <label htmlFor="port">Port:</label>
           <input type="text" id="port" value={this.getProp('port')} placeholder="6379" />
         </div>
         <div className="nt-form__row">
-          <label htmlFor="password">Password</label>
+          <label htmlFor="password">Password:</label>
           <input type="password" id="password" value={this.getProp('password')} />
         </div>
         <div className="nt-form__row">
-          <label htmlFor="ssh">SSH Tunnel</label>
+          <label htmlFor="ssh">SSH Tunnel:</label>
           <input type="checkbox" id="ssh" value={this.getProp('ssh')} />
         </div>
-        <button onPress={() => {
+        <button className="nt-button" onPress={() => {
         }}>Save Changes</button>
-        <button onPress="submit" color="blue">Save and Connect</button>
+        <button className="nt-button nt-button--primary" onPress="submit">Save and Connect</button>
       </form>
     </div>;
   }
