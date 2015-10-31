e334dcb8c1aec853d9eb8e5f94dcc0d074890454
luin <i@zihua.li>
2015-10-31 14:38:07 +0800

    Fix

--- client/components/main/Main/Database/index.jsx
+++ client/components/main/Main/Database/index.jsx
@@ -4,57 +4,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from '../../../../store';
 import action from '../../../../actions';
-import { Table, Column } from 'fixed-data-table';
 import SplitPane from 'react-split-pane';
 import PatternList from './PatternList';
+import KeyList from './KeyList';
+require('./index.scss');
 
 class Database extends React.Component {
   constructor() {
     super();
+    this.footerHeight = 66;
+
     this.state = {
-      keys: [],
-      selectedKey: null,
       sidebarWidth: 300,
-      cursor: 0
+      clientHeight: $(window).height() - this.footerHeight,
+      pattern: ''
     };
   }
 
   componentDidMount() {
-    const redis = this.props.redis;
-
-    redis.scan('0', 'MATCH', '*', 'COUNT', '20000', (_, res) => {
-      Promise.all(res[1].map(key => {
-        return Promise.all([key, redis.type(key)]);
-      })).then(keys => {
-        this.setState({
-          keys
-        });
-      });
-    });
-
     window.addEventListener('resize', this._update.bind(this), false);
     this._update();
   }
 
   _update() {
     const node = ReactDOM.findDOMNode(this);
-
-    window.node = node;
-    this.setState({
-      windowWidth: node.clientWidth,
-      windowHeight: node.clientHeight
-    });
-
-    this.setState({ dropdownHeight: node.clientHeight - 66 });
+    this.setState({ clientHeight: node.clientHeight - this.footerHeight });
   }
 
   handleSelectPattern() {
   }
 
   render() {
-    function rowGetter(rowIndex) {
-      return this.state.keys[rowIndex] || [];
-    }
     return <SplitPane
         className="pane-group"
         minSize="100"
@@ -62,68 +42,28 @@ class Database extends React.Component {
         defaultSize={300}
         ref="node"
         onChange={size => {
-          this.setState({ 'sidebarWidth': size });
+          this.setState({ sidebarWidth: size });
         }}
       >
       <div className="pane sidebar">
         <PatternList
           patterns={ this.props.patterns }
-          height={ this.dropdownHeight }
+          height={ this.state.clientHeight }
+          onChange={pattern => {
+            console.log('pattern', pattern);
+            this.setState({ pattern });
+          }}
         />
-        <div className="pattern-table">
-          <Table
-            rowHeight={24}
-            rowGetter={rowGetter.bind(this)}
-            rowsCount={this.state.keys.length + 1}
-            rowClassNameGetter={index => {
-              const item = this.state.keys[index];
-              if (!item) {
-                return 'is-loading';
-              }
-              if (item[0] === this.state.selectedKey) {
-                return 'is-selected';
-              }
-              return '';
-            }}
-            onRowClick={(evt, index) => {
-              const item = this.state.keys[index];
-              if (item && item[0]) {
-                this.setState({ selectedKey: item[0] });
-              }
-            }}
-            width={this.state.sidebarWidth}
-            height={this.state.windowHeight - 66}
-            headerHeight={24}
-          >
-            <Column
-              label="type"
-              width={40}
-              dataKey={1}
-              cellRenderer={
-                cellData => {
-                  if (!cellData) {
-                    return '';
-                  }
-                  const type = cellData === 'string' ? 'str' : cellData;
-                  return <span className={`key-type ${type}`}>{type}</span>;
-                }
-              }
-            />
-            <Column
-              label="name"
-              width={this.state.sidebarWidth - 50}
-              dataKey={0}
-              cellRenderer={
-                cellData => {
-                  if (!cellData) {
-                    return 'Scanning...';
-                  }
-                  return cellData;
-                }
-              }
-            />
-          </Table>
-        </div>
+        <KeyList
+          height={ this.state.clientHeight }
+          width= { this.state.sidebarWidth }
+          db={ 0 }
+          pattern={ this.state.pattern || '*' }
+          redis={ this.props.redis }
+          onSelect={key => {
+            console.log(`Select ${key}`);
+          }}
+        />
         <footer className="toolbar toolbar-footer">
         </footer>
       </div>
