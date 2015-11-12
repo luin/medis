566ce2f43b6cbc849429004187b2f741504a2d47
Zihua Li <i@zihua.li>
2015-11-12 20:56:13 +0800

    Add buttons

--- client/components/main/Main/Database/Content/KeyContent/StringContent.jsx
+++ client/components/main/Main/Database/Content/KeyContent/StringContent.jsx
@@ -19,26 +19,65 @@ require('./StringContent.scss');
 class StringContent extends BaseContent {
   constructor() {
     super();
-    this.state.content = '';
+    this.state.currentMode = null;
+    this.state.wrapping = true;
+    this.state.modes = {
+      raw: false,
+      json: false,
+      messagepack: false
+    };
   }
 
   init(keyName) {
     this.setState({ content: '' });
     this.props.redis.get(keyName, (err, content) => {
-      content = JSON.stringify(JSON.parse(content), null, '\t');
-      this.setState({ content });
+      this.state.modes.raw = content;
+      this.state.modes.json = tryFormatJSON(content);
+      const mode = typeof this.state.modes.json === 'string' ? 'json' : 'raw';
+      this.setState({ content, currentMode: mode });
     });
   }
 
-  updateContent(content) {
-    this.setState({ content });
+  updateContent(mode, content) {
+    let json, raw;
+    if (mode === 'raw') {
+      raw = content;
+      json = tryFormatJSON(content);
+    } else if (mode === 'json') {
+      json = content;
+      try {
+        raw = JSON.stringify(JSON.parse(json));
+      } catch (e) {
+        raw = this.state.modes.raw;
+      }
+    }
+    this.setState({ modes: { json, raw } });
+  }
+
+  updateMode(evt) {
+    const newMode = evt.target.value;
+    this.setState({ currentMode: newMode });
   }
 
   render() {
-    return <div style={{ height: this.props.height }} className="StringContent">
-      <Codemirror
-        value={this.state.content}
-        onChange={this.updateContent.bind(this)}
+    let viewer;
+    if (this.state.currentMode === 'raw') {
+      viewer = <Codemirror
+        key="raw"
+        value={this.state.modes.raw}
+        onChange={this.updateContent.bind(this, 'raw')}
+        options={{
+          mode: 'none',
+          styleActiveLine: true,
+          lineWrapping: this.state.wrapping,
+          lineNumbers: true
+        }}
+      />;
+    } else if (this.state.currentMode === 'json') {
+      viewer = <Codemirror
+        key="json"
+        value={this.state.modes.json}
+        onChange={this.updateContent.bind(this, 'json')}
         options={{
           mode: {
             name: 'javascript',
@@ -48,14 +87,50 @@ class StringContent extends BaseContent {
           indentWithTabs: true,
           styleActiveLine: true,
           lineNumbers: true,
+          lineWrapping: this.state.wrapping,
           gutters: ["CodeMirror-lint-markers"],
           autoCloseBrackets: true,
           matchTags: true,
-          lint: !!this.state.content
+          lint: !!this.state.modes.raw
         }}
       />
+    } else {
+      viewer = <div></div>;
+    }
+    return <div style={{ height: this.props.height }} className="StringContent">
+      <label className="wrap-selector">
+        <input
+          type="checkbox"
+          checked={this.state.wrapping}
+          onChange={evt => this.setState({ wrapping: evt.target.checked })}
+        />
+        <span>Wrapping</span>
+      </label>
+      <select
+        className="mode-selector"
+        value={this.state.currentMode}
+        onChange={this.updateMode.bind(this)}
+      >
+        <option value="raw" disabled={typeof this.state.modes.raw !== 'string'}>Raw</option>
+        <option value="json" disabled={typeof this.state.modes.json !== 'string'}>JSON</option>
+        <option disabled>MessagePack</option>
+      </select>
+      <button className="nt-button">Save Changes</button>
+      { viewer }
     </div>;
   }
 }
 
 export default StringContent;
+
+function tryFormatJSON(jsonString) {
+  try {
+    const o = JSON.parse(jsonString);
+    if (o && typeof o === "object" && o !== null) {
+      return JSON.stringify(o, null, '\t');
+    }
+  }
+  catch (e) { }
+
+  return false;
+}
