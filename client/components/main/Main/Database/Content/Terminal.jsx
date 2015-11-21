5eaba5311badc9a443253b1620e2a45c3c9589ae
luin <i@zihua.li>
2015-11-21 23:07:56 +0800

    Init terminal

--- client/components/main/Main/Database/Content/Terminal.jsx
+++ client/components/main/Main/Database/Content/Terminal.jsx
@@ -1,17 +1,24 @@
 'use strict';
 
 import React from 'react';
+import commands from 'redis-commands';
 
 require('./TabBar.scss');
 
 class Terminal extends React.Component {
   componentDidMount() {
+    const redis = this.props.redis;
     $(this.refs.terminal).terminal((command, term) => {
-      console.log(command, term);
+      console.log();
+      redis.call.apply(redis, command.split(' ')).then(res => {
+        term.echo(res);
+      }).catch(err => {
+        console.log(err);
+      });
     }, {
       greetings: 'Javascript Interpreter',
       exit: false,
-      completion: ['CLUSTER', 'SADD'],
+      completion: commands.list.concat(commands.list.map(c => c.toUpperCase())),
       name: 'Redis Terminal',
       height: '100%',
       width: '100%',
