3e8670f6ad227fa2ed41edfbddb16632e9e733da
Zihua Li <i@zihua.li>
2015-11-27 13:13:13 +0800

    Fix zset

--- client/components/main/Main/Database/Content/KeyContent/ZSetContent.jsx
+++ client/components/main/Main/Database/Content/KeyContent/ZSetContent.jsx
@@ -3,10 +3,13 @@
 import React from 'react';
 import BaseContent from './BaseContent';
 import SplitPane from 'react-split-pane';
-import { Table, Column } from 'fixed-data-table';
+import { Table, Column } from 'fixed-data-table-contextmenu';
 import Editor from './Editor';
 import SortHeaderCell from './SortHeaderCell';
 import AddButton from '../../../../../common/AddButton';
+import ContentEditable from '../../../../../common/ContentEditable';
+import ReactDOM from 'react-dom';
+import { clipboard } from 'electron';
 
 require('./BaseContent.scss');
 
@@ -62,6 +65,87 @@ class ZSetContent extends BaseContent {
     }
   }
 
+  handleKeyDown(e) {
+    if (typeof this.state.selectedIndex === 'number' && typeof this.state.editableIndex !== 'number') {
+      if (e.keyCode === 8) {
+        this.deleteSelectedMember();
+        return false;
+      }
+      if (e.keyCode === 38) {
+        if (this.state.selectedIndex > 0) {
+          this.handleSelect(null, this.state.selectedIndex - 1);
+        }
+        return false;
+      }
+      if (e.keyCode === 40) {
+        if (this.state.selectedIndex < this.state.members.length - 1) {
+          this.handleSelect(null, this.state.selectedIndex + 1);
+        }
+        return false;
+      }
+    }
+  }
+
+  deleteSelectedMember() {
+    if (typeof this.state.selectedIndex !== 'number') {
+      return;
+    }
+    showModal({
+      title: 'Delete selected item?',
+      button: 'Delete',
+      content: 'Are you sure you want to delete the selected item? This action cannot be undone.'
+    }).then(() => {
+      const members = this.state.members;
+      const deleted = members.splice(this.state.selectedIndex, 1);
+      if (deleted.length) {
+        this.props.redis.zrem(this.state.keyName, deleted[0]);
+        if (this.state.selectedIndex >= members.length - 1) {
+          this.state.selectedIndex -= 1;
+        }
+        this.setState({ members, length: this.state.length - 1 }, () => {
+          this.handleSelect(null, this.state.selectedIndex);
+        });
+      }
+    });
+  }
+
+  componentDidMount() {
+    super.componentDidMount();
+    $.contextMenu({
+      context: ReactDOM.findDOMNode(this.refs.table),
+      selector: '.' + this.randomClass,
+      trigger: 'none',
+      zIndex: 99999,
+      callback: (key, opt) => {
+        setTimeout(() => {
+          if (key === 'delete') {
+            this.deleteSelectedMember();
+          } else if (key === 'copy') {
+            clipboard.writeText(this.state.members[this.state.selectedIndex][0]);
+          } else if (key === 'edit') {
+            this.setState({ editableIndex: this.state.selectedIndex});
+          }
+        }, 0);
+        ReactDOM.findDOMNode(this.refs.table).focus();
+      },
+      items: {
+        copy: { name: 'Copy Score to Clipboard'},
+        sep1: '---------',
+        edit: { name: 'Edit Score'},
+        delete: { name: 'Delete' }
+      }
+    });
+  }
+
+  showContextMenu(e, row) {
+    this.handleSelect(null, row);
+    $(ReactDOM.findDOMNode(this.refs.table)).contextMenu({
+      x: e.pageX,
+      y: e.pageY,
+      zIndex: 99999
+    });
+  }
+
   render() {
     return <SplitPane
       className="pane-group"
@@ -73,12 +157,23 @@ class ZSetContent extends BaseContent {
         this.setState({ sidebarWidth: size });
       }}
       >
-      <div style={ { 'marginTop': -1 } }>
+      <div
+        style={ { 'marginTop': -1 } }
+        onKeyDown={this.handleKeyDown.bind(this)}
+        tabIndex="0"
+        ref="table"
+        className={this.randomClass}
+      >
         <Table
           rowHeight={24}
           rowsCount={this.state.length}
           rowClassNameGetter={this.rowClassGetter.bind(this)}
           onRowClick={this.handleSelect.bind(this)}
+          onRowContextMenu={this.showContextMenu.bind(this)}
+          onRowDoubleClick={(evt, index) => {
+            this.handleSelect(evt, index);
+            this.setState({ editableIndex: index });
+          }}
           isColumnResizing={false}
           onColumnResizeEndCallback={ scoreWidth => {
             this.setState({ scoreWidth });
@@ -105,7 +200,34 @@ class ZSetContent extends BaseContent {
               if (!member) {
                 return '';
               }
-              return <div className="overflow-wrapper"><span>{member[1]}</span></div>;
+              return <ContentEditable
+                className="ContentEditable overflow-wrapper"
+                enabled={rowIndex === this.state.editableIndex}
+                onChange={newScore => {
+                  const members = this.state.members;
+                  const member = members[rowIndex];
+                  const keyName = this.state.keyName;
+                  this.props.redis.zadd(keyName, newScore, member[0]).then(() => {
+                    member[1] = newScore;
+                    const updatedMembers = members.sort((a, b) => {
+                      return Number(a[1]) - Number(b[1]);
+                    });
+                    this.setState({
+                      members: updatedMembers
+                    }, () => {
+                      for (let i = 0; i < updatedMembers.length; i++) {
+                        if (updatedMembers[i][0] === member[0]) {
+                          this.handleSelect(null, i);
+                          break;
+                        }
+                      }
+                    });
+                  });
+                  this.setState({ editableIndex: null });
+                  ReactDOM.findDOMNode(this).focus();
+                }}
+                html={member[1]}
+              />;
             } }
           />
           <Column
