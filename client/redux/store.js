e0949adc94badf3bd95303b2c09605cdda95b355
luin <i@zihua.li>
2019-01-05 16:34:25 +0800

    pref: reduce file size

--- client/redux/store.js
+++ client/redux/store.js
@@ -2,11 +2,10 @@ import {compose, createStore, applyMiddleware} from 'redux'
 import persistEnhancer from './persistEnhancer'
 import {createThunkReplyMiddleware} from 'Redux/middlewares'
 import reducers from './reducers'
-import logger from 'redux-logger'
 
 const store = window.store = createStore(
   reducers,
-  applyMiddleware(logger, createThunkReplyMiddleware())
+  applyMiddleware(createThunkReplyMiddleware())
 )
 
 persistEnhancer(store)
