ca130768b51d3af4d588be8ba805aeaaef128fed
luin <i@zihua.li>
2019-01-05 17:51:37 +0800

    fix: hide tabs bar when there's only one tab

--- client/windows/MainWindow/components/InstanceTabs/Tabs.js
+++ client/windows/MainWindow/components/InstanceTabs/Tabs.js
@@ -1,28 +1,26 @@
 'use strict';
 
-import React from 'react';
+import React, {memo} from 'react';
 import {SortableContainer} from 'react-sortable-hoc';
 import Tab from './Tab'
 
-class Tabs extends React.Component {
-  render() {
-    const {instances, activeInstanceKey, onTabSelect, onTabClose} = this.props
-    return (
-      <div style={{display: 'flex', flex: 1}}>
-        {instances.map((instance, index) => {
-          return <Tab
-            key={instance.get('key')}
-            instanceKey={instance.get('key')}
-            index={index}
-            title={instance.get('title')}
-            active={activeInstanceKey === instance.get('key')}
-            onTabClick={onTabSelect}
-            onTabCloseButtonClick={onTabClose}
-          />
-        })}
-      </div>
-    )
-  }
+function Tabs({instances, activeInstanceKey, onTabSelect, onTabClose}) {
+  return (
+    <div style={{display: 'flex', flex: 1}}>
+      {instances.map((instance, index) => {
+        const key = instance.get('key')
+        return <Tab
+          key={key}
+          instanceKey={key}
+          index={index}
+          title={instance.get('title')}
+          active={activeInstanceKey === key}
+          onTabClick={onTabSelect}
+          onTabCloseButtonClick={onTabClose}
+        />
+      })}
+    </div>
+  )
 }
 
-export default SortableContainer(Tabs);
+export default memo(SortableContainer(Tabs));
