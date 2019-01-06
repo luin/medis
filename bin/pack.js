f692e6d192ba11bb710f114a88e61899bfc33d24
luin <i@zihua.li>
2019-01-06 15:59:28 +0800

    refactor: more robust build config

--- bin/pack.js
+++ bin/pack.js
@@ -3,21 +3,23 @@ const path = require('path')
 const pkg = require('../package')
 const flat = require('electron-osx-sign').flat
 
+const resourcesPath = path.join(__dirname, '..', 'resources')
+
 packager({
   dir: path.join(__dirname, '..'),
   appCopyright: '© 2019, Zihua Li',
   asar: true,
   overwrite: true,
   electronVersion: pkg.electronVersion,
-  icon: path.join(__dirname, '..', 'icns', 'MyIcon'),
-  out: path.join(__dirname, '..', 'out'),
+  icon: path.join(resourcesPath, 'icns', 'MyIcon'),
+  out: path.join(__dirname, '..', 'dist', 'out'),
   platform: 'mas',
   appBundleId: `li.zihua.${pkg.name}`,
   appCategoryType: 'public.app-category.developer-tools',
   osxSign: {
     type: process.env.NODE_ENV === 'production' ? 'distribution' : 'development',
-    entitlements: path.join(__dirname, '..', 'parent.plist'),
-    'entitlements-inherit': path.join(__dirname, '..', 'child.plist')
+    entitlements: path.join(resourcesPath, 'parent.plist'),
+    'entitlements-inherit': path.join(resourcesPath, 'child.plist')
   }
 }).then((res) => {
   const app = path.join(res[0], `${pkg.productName}.app`)
--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,108 @@
 'use strict';
 
-const path = require('path')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const {resolve} = require('path')
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development'
+const watch = process.env.WEBPACK_WATCH === 'true'
 
-module.exports = {
-  mode,
-  watch: process.env.WEBPACK_WATCH === 'true',
-  entry: {
-    main: './client/windows/MainWindow/entry.jsx',
-    patternManager: './client/windows/PatternManagerWindow/entry.jsx'
-  },
-  node: {
-    Buffer: false,
-    buffer: false
-  },
+const distPath = resolve(__dirname, 'dist')
+const base = {
+  mode, watch,
   output: {
+    path: distPath,
     chunkFilename: '[name].chunk.js',
     filename: '[name].js'
   },
-  plugins: mode === 'production' ? [] : [
-    new BundleAnalyzerPlugin()
-  ],
+  node: {
+    Buffer: false,
+    buffer: false,
+    __dirname: false,
+    __filename: false,
+  },
   module: {
     rules: [{
-      test: /\.jsx$/,
+      test: /\.(js|jsx)$/,
       exclude: /node_modules/,
       use: [{
         loader: 'babel-loader',
         options: {ignore: ['buffer']}
       }]
     }, {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: [{
-        loader: 'babel-loader',
-        options: {ignore: ['buffer']}
-      }]
+      test: /\.(ts|tsx)?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
     }, {
       test: /\.scss$/,
       use: [
-        'style-loader',
+        MiniCssExtractPlugin.loader,
         'css-loader',
         'sass-loader'
       ]
     }, {
       test: /\.css$/,
       use: [
-        'style-loader',
+        MiniCssExtractPlugin.loader,
         'css-loader'
       ]
     }, {
       test: /\.(png|jpg)$/,
       use: [{
-        loader: "url-loader"
+        loader: "file-loader"
+      }]
+    }, {
+      test: /\.(eot|woff|ttf)$/,
+      use: [{
+        loader: "file-loader"
       }]
     }]
   },
   externals: {
-    'ioredis': 'require("ioredis")',
-    'electron': 'require("electron")',
-    'redis-commands': 'require("redis-commands")',
-    'ssh2': 'require("ssh2")',
-    'net': 'require("net")',
-    'remote': 'require("remote")',
-    'shell': 'require("shell")',
-    'app': 'require("app")',
-    'ipc': 'require("ipc")',
-    'fs': 'require("fs")',
-    'buffer': 'require("buffer")',
-    'system': '{}',
-    'file': '{}'
+    'system': '{}', // jsonlint
+    'file': '{}' // jsonlint
   },
+}
+
+const renderPlugins = [
+  new HtmlWebpackPlugin({title: 'Medis', chunks: ['main'], filename: 'main.html'}),
+  new HtmlWebpackPlugin({title: 'Manage Patterns', chunks: ['patternManager'], filename: 'patternManager.html'}),
+  new MiniCssExtractPlugin({ filename: '[name].css' })
+]
+if (mode === 'production') {
+  renderPlugins.push(new BundleAnalyzerPlugin())
+}
+const renderer = Object.assign({}, base, {
+  target: 'electron-renderer',
+  output: Object.assign({}, base.output, {
+    path: resolve(base.output.path, 'renderer')
+  }),
+  entry: {
+    main: resolve(__dirname, 'src/renderer/windows/MainWindow/entry.jsx'),
+    patternManager: resolve(__dirname, 'src/renderer/windows/PatternManagerWindow/entry.jsx')
+  },
+  plugins: renderPlugins,
   resolve: {
     alias: {
-      Redux: path.resolve(__dirname, 'client/redux/'),
-      Utils: path.resolve(__dirname, 'client/utils/'),
+      Redux: resolve(__dirname, 'src/renderer/redux/'),
+      Utils: resolve(__dirname, 'src/renderer/utils/'),
     },
     extensions: ['.js', '.jsx']
   }
-}
+})
+
+const main = Object.assign({}, base, {
+  target: 'electron-main',
+  output: Object.assign({}, base.output, {
+    path: resolve(base.output.path, 'main')
+  }),
+  entry: {
+    index: resolve(__dirname, 'src/main/index.js')
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  }
+})
+
+module.exports = [main, renderer]
